8623d6e70edfc42d7c86e56f022ec97bc89afb3d
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-03-06 11:30:02 +0300

    Create the total of the player

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic
 //Randomizing the dice number
-var diceNumber, totalNumber1, totalNumber2, chance1;
-diceNumber, totalNumber2, chance1, totalNumber1 = 0;
+var diceNumber, totalNumber1, totalNumber2, chance1, player1;
+diceNumber, totalNumber2, chance1, totalNumber1, player1 = 0;
+totalNumber1 = 0;
 //event.preventDefault();
 
 function Details(firstName, secondName){
@@ -21,11 +22,18 @@ $(document).ready(function(){
       $('#score1').text(chance1)
       $('#totalScore').text(totalNumber1)
       $('#rollmoto').text('You rolled a one. It is time for player two to play!').addClass('text-dark text-centre jumbotron')
+      $('#one').hide(100);
+      player1 += totalNumber1;
+      $('#player-1-total').text(player1)
     }else {
       $('#score1').text(chance1)
       totalNumber1 += chance1 ;
       $('#totalScore').text(totalNumber1)
     }
   })
-  $('#hold1')
+  $('#hold1').click(function(){
+    player1 += totalNumber1;
+    $('#one').hide(100);
+    $('#player-1-total').text(player1)
+  })
 })
