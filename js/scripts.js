308b5a97ed6589c38e938ae8384ff1c204135e54
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-03-07 07:38:44 +0300

    Display winner

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 //Business Logic
 //Randomizing the dice number
-var diceNumber, totalNumber1, totalNumber2, chance1, player1;
-diceNumber, totalNumber2, chance1, totalNumber1, player1 = 0;
+var diceNumber, totalNumber1, totalNumber2, chance1, player1, chance2, player2;
 totalNumber1 = 0;
+player1 = 0;
+totalNumber2 = 0;
+player2 = 0;
 //event.preventDefault();
 
 function Details(firstName, secondName){
   firstName=this.firstName;
   secondName=this.secondName
 }
+/*var player1 = function(){
+  $('')
+}*/
 
 $(document).ready(function(){
   $('#try').click(function(){
@@ -24,6 +29,7 @@ $(document).ready(function(){
       $('#totalScore').text(totalNumber1)
       $('#rollmoto').text('You rolled a one. It is time for player two to play!').addClass('text-dark text-centre jumbotron alert-danger')
       $('#one').hide(100);
+      $('#two').show(100);
       player1 += totalNumber1;
       $('#player-1-total').text(player1)
     }else {
@@ -35,8 +41,37 @@ $(document).ready(function(){
   $('#hold1').click(function(){
     player1 += totalNumber1;
     $('#one').hide(100);
+    $('#two').show(100);
     $('#player-1-total').text(player1)
-    $('#rollmoto').text('It is time for player two to play.').addClass('text-dark text-centre jumbotron ')
+    $('#rollmoto').text('It is time for player two to play.').addClass('text-dark text-centre jumbotron alert-info').removeClass('alert-danger')
+    $('#rollmoto2').text('It is your turn, play wisely.').addClass('text-dark text-centre jumbotron alert-success').removeClass('alert-danger alert-info')
+    totalNumber1 = 0;
+  })
+  //Player 2
+  $('#roll2').click(function(){
+    chance2 =  Math.floor((Math.random() * 6) + 1);
+    if (chance2 == 1){
+      totalNumber2 = 0;
+      $('#score2').text(chance2)
+      $('#totalScore2').text(totalNumber2)
+      $('#rollmoto2').text('You rolled a one. It is time for player one to play!').addClass('text-dark text-centre jumbotron alert-danger')
+      $('#two').hide(100);
+      $('#one').show(100)
+      player2 += totalNumber2;
+      $('#player-2-total').text(player2)
+    }else {
+      $('#score2').text(chance2)
+      totalNumber2 += chance2 ;
+      $('#totalScore2').text(totalNumber2)
+    }
+  })
+  $('#hold2').click(function(){
+    player2 += totalNumber2;
+    $('#two').hide(100);
+    $('#one').show(100); 
+    $('#player-2-total').text(player2)
+    $('#rollmoto2').text(`It is time for player one to play.`).addClass('text-dark text-centre jumbotron alert-info').removeClass('alert-danger')
+    totalNumber2 = 0;
   })
 })
 
